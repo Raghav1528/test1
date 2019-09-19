944c6d5de165146b88041e1b47958cdbd32b4aa1
cheroliv <cheroliv@laposte.net>
2019-09-19 22:31:47 +0200

    Initial commit

---
+++ src/main/groovy/com/cheroliv/staticsite/StaticSiteApplication.groovy
@@ -0,0 +1,13 @@
+package com.cheroliv.staticsite
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class StaticSiteApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(StaticSiteApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/cheroliv/staticsite/StaticSiteApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.cheroliv.staticsite
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class StaticSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
